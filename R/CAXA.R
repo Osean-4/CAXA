821aaa8981bca950559863df9f4d3d926385492f
MacDonald <erroll4@illinois.edu>
2017-12-11 17:33:24 -0600

    6

--- R/CAXA.R
+++ R/CAXA.R
@@ -1,17 +1,17 @@
 # Remove all experiments that include a track species of "wrsh" and "zedo"
 Remove.WRSH.ZEDO <- function(cax_data){
   cax_data_1 <- cax_data %>%
-  group_by(experiment) %>%
-  filter(!any(track.spp == "wrsh")) %>%
-  filter(!any(track.spp == "zedo"))
+  dplyr::group_by(experiment) %>%
+  dplyr::filter(!any(track.spp == "wrsh")) %>%
+  dplyr::filter(!any(track.spp == "zedo"))
   print(cax_data_1)
 }
 
 # Keep only experiments where all trials have a track length = 15 minutes
 Keep.15.Tracks <- function(cax_data_1){
   cax_data_2 <- cax_data_1 %>%
-  group_by(experiment) %>%
-  filter(!any(track.length != "15"))
+  dplyr::group_by(experiment) %>%
+  dplyr::filter(!any(track.length != "15"))
   print(cax_data_2)
 }
 
@@ -19,6 +19,6 @@ Keep.15.Tracks <- function(cax_data_1){
 # For each glmm have bird species as response variable; track species, breeding season, and plant origin as fixed effects; order as random effect
 GLMM.Bin <- function(cax_data_2){
   species <- cax_data_2$jawe
-  model <- glmer(species ~ track.spp + breeding.season + native.plant + (1 | track.order), data = cax_data_2, family = binomial)
+  model <- glmer(species ~ track.spp + breeding.season + native.plant + (1 | track.order), data = cax_data_2, family = "binomial")
   summary(model)
 }
