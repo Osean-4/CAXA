446b080df5cbe0df7a588cfc9c4352f62e20f89e
MacDonald <erroll4@illinois.edu>
2017-12-13 17:30:37 -0600

    12

--- R/CAXA.R
+++ R/CAXA.R
@@ -1,26 +1,35 @@
 # Remove all experiments that include a track species of "wrsh" and "zedo"
-Remove.WRSH.ZEDO <- function(cax_data){
+Remove.WRSH.ZEDO <- function(cax_data, track.spp.remove = "wrsh" | "zedo"){
   experiment <- track.spp <- NULL
   cax_data_1 <- cax_data %>%
   dplyr::group_by(experiment) %>%
   dplyr::filter(!any(track.spp == "wrsh")) %>%
   dplyr::filter(!any(track.spp == "zedo"))
-  print(cax_data_1)
+  return(cax_data_1)
 }
   
 # Keep only experiments where all trials have a track length = 15 minutes
-Keep.15.Tracks <- function(cax_data_1){
+Keep.15.Tracks <- function(cax_data_1, track.length.keep = "15"){
   experiment <- track.length <- NULL
   cax_data_2 <- cax_data_1 %>%
   dplyr::group_by(experiment) %>%
-  dplyr::filter(!any(track.length != "15"))
-  print(cax_data_2)
+  dplyr::filter(!any(!track.length %in% track.length.keep))
+  return(cax_data_2)
 }
 
-# Apply a generalized linear mixed model with repeated measures and a binomial distribution
-# For each glmm have bird species as response variable; track species, breeding season, and plant origin as fixed effects; order as random effect
+# Apply a generalized linear mixed model (GLMM) with a binomial distribution
+# For each GLMM bird species is the response variable; track species, 
+# breeding season, and plant origin are fixed effects; order is random effect
 GLMM.Bin <- function(cax_data_2){
-  species <- cax_data_2$jawe
-  model <- glmer(species ~ track.spp + breeding.season + native.plant + (1 | track.order), data = cax_data_2, family = "binomial")
-  summary(model)
+  model1 <- glmer(jawe ~ track.spp + breeding.season + native.plant + 
+                    (1 | track.order), data = cax_data_2, family = "binomial")
+  model2 <- glmer(rble ~ track.spp + breeding.season + native.plant + 
+                    (1 | track.order), data = cax_data_2, family = "binomial")
+  model3 <- glmer(rvbu ~ track.spp + breeding.season + native.plant + 
+                    (1 | track.order), data = cax_data_2, family = "binomial")
+  model4 <- glmer(rwbu ~ track.spp + breeding.season + native.plant + 
+                    (1 | track.order), data = cax_data_2, family = "binomial")
+  model5 <- glmer(other ~ track.spp + breeding.season + native.plant + 
+                    (1 | track.order), data = cax_data_2, family = "binomial")
+  return(c(model1, model2, model3, model4, model5))
 }
