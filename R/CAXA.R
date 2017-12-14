189b310abd033ec9d10255c9211b7a5b50efae02
MacDonald <erroll4@illinois.edu>
2017-12-13 21:49:45 -0600

    13

--- R/CAXA.R
+++ R/CAXA.R
@@ -1,4 +1,6 @@
-# Remove all experiments that include a track species of "wrsh" and "zedo"
+## Remove all experiments with track species containing wrsh or 
+## zedo. The newly cleaned dataframe (cax_data_1) should contain 
+## 573 observations now.
 Remove.WRSH.ZEDO <- function(cax_data, track.spp.remove = "wrsh" | "zedo"){
   experiment <- track.spp <- NULL
   cax_data_1 <- cax_data %>%
@@ -8,7 +10,10 @@ Remove.WRSH.ZEDO <- function(cax_data, track.spp.remove = "wrsh" | "zedo"){
   return(cax_data_1)
 }
   
-# Keep only experiments where all trials have a track length = 15 minutes
+## Utilizing the cleaned dataframe (cax_data_1) from 
+## Remove.WRSH.ZEDO, this function will now Keep only experiments 
+## with all trials of track lengths equal to 15 minutes. The newly 
+## cleaned dataframe (cax_data_2) should contain 448 observations now.
 Keep.15.Tracks <- function(cax_data_1, track.length.keep = "15"){
   experiment <- track.length <- NULL
   cax_data_2 <- cax_data_1 %>%
@@ -17,9 +22,14 @@ Keep.15.Tracks <- function(cax_data_1, track.length.keep = "15"){
   return(cax_data_2)
 }
 
-# Apply a generalized linear mixed model (GLMM) with a binomial distribution
-# For each GLMM bird species is the response variable; track species, 
-# breeding season, and plant origin are fixed effects; order is random effect
+## Utilizing the latest cleaned dataframe (cax_data_2), this function 
+## applys a generalized linear mixed model with a binomial distribution.
+## Bird species is the response variable; track species, breeding season, 
+## and plant origin as fixed effects; order as the one random effect. 
+## The output shows that the track species call's jawe, jawe.rble, rble, 
+## and rvbu significantly attract Japanese white-eyes in comparison to 
+## the intercept (i.e. control). The breeding season and plant origin did 
+## not signigicantly influence strength of response.
 GLMM.Bin <- function(cax_data_2){
   model1 <- glmer(jawe ~ track.spp + breeding.season + native.plant + 
                     (1 | track.order), data = cax_data_2, family = "binomial")
